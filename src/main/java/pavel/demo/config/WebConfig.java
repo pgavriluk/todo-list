8f2655d448cd0fbedf3261ce7f0fe8bd6347c15e
Pavel <pavel.gavriluk@collibra.com>
2019-05-20 15:33:07 -0400

    adding WebConfig and dispatcher servlet

---
+++ src/main/java/pavel/demo/config/WebConfig.java
@@ -0,0 +1,11 @@
+package pavel.demo.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@EnableWebMvc
+@Configuration
+@ComponentScan(basePackages = "pavel.demo")
+public class WebConfig {
+}
